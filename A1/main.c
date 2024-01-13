98cecc14e44a8a446c885f65420da4df7098c7b8
dsandall <91931921+dsandall@users.noreply.github.com>
2024-01-12 20:05:49 -0800

    Assignment 1
    3 Bit LED counter for STM32. 
    Performance Benchmarks.

---
+++ A1/main.c
@@ -0,0 +1,177 @@
+#include "main.h"
+
+void SystemClock_Config(void);
+void mydelay(int count);
+int main(void)
+{
+  HAL_Init();
+  SystemClock_Config();
+
+
+// turns on clock to GPIO banks A and C
+RCC->AHB2ENR |= (RCC_AHB2ENR_GPIOAEN | RCC_AHB2ENR_GPIOCEN);
+
+// bank A as GPIO mode
+GPIOC->MODER &= ~(GPIO_MODER_MODE0);
+GPIOC->MODER &= ~(GPIO_MODER_MODE1);
+GPIOC->MODER &= ~(GPIO_MODER_MODE2);
+GPIOC->MODER &= ~(GPIO_MODER_MODE3);
+
+GPIOA->MODER &= ~(GPIO_MODER_MODE5);
+GPIOA->MODER |= (GPIO_MODER_MODE5_0);
+
+GPIOC->MODER |= (GPIO_MODER_MODE0_0);
+GPIOC->MODER |= (GPIO_MODER_MODE1_0);
+GPIOC->MODER |= (GPIO_MODER_MODE2_0);
+GPIOC->MODER |= (GPIO_MODER_MODE3_0);
+
+// bank C as GPIO mode
+//GPIOC->MODER &= ~(GPIO_MODER_MODE13);
+//GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPD13);
+
+GPIOA->MODER &= ~(GPIO_MODER_MODE4);
+GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPD4);
+GPIOA->PUPDR |= (GPIO_PUPDR_PUPD4_0);
+
+   while (1)
+  {
+
+		   uint8_t i = 0;
+		   while (i < 8)
+		   {
+			   if (i & 1){
+				   GPIOC->ODR |= GPIO_PIN_0;
+			   } else{
+				   GPIOC->ODR &= ~GPIO_PIN_0;
+			   }
+
+			   if (i & 2) {
+				   GPIOC->ODR |= GPIO_PIN_1;
+			   } else{
+				   GPIOC->ODR &= ~GPIO_PIN_1;
+			   }
+
+			   if (i & 4){
+				   GPIOC->ODR |= GPIO_PIN_2;
+			   } else{
+				   GPIOC->ODR &= ~GPIO_PIN_2;
+			   }
+			   HAL_Delay(200);
+
+			   if (GPIOA->IDR & GPIO_PIN_4)
+			   {
+				   i++;
+			   }
+			   else
+			   {
+				   i = i;
+			   }
+		   }
+
+
+  }
+
+}// end main
+
+void mydelay(int count)
+{
+	int i, j;
+	int x = 0;
+	for(i=0; i<count; i++)
+		for(j=0; j<1000; j++)  // need to tune this?
+			x = x + 1;
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
+  //RCC_OscInitStruct.MSIState = RCC_MSI_ON;  //datasheet says NOT to turn on the MSI then change the frequency.
+  RCC_OscInitStruct.MSICalibrationValue = 0;
+  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_10;
+	/* from stm32l4xx_hal_rcc.h==
+	#define RCC_MSIRANGE_0                 MSI = 100 KHz
+	#define RCC_MSIRANGE_1                 MSI = 200 KHz
+	#define RCC_MSIRANGE_2                 MSI = 400 KHz
+	#define RCC_MSIRANGE_3                 MSI = 800 KHz
+	#define RCC_MSIRANGE_4                 MSI = 1 MHz
+	#define RCC_MSIRANGE_5                 MSI = 2 MHz
+	#define RCC_MSIRANGE_6                 MSI = 4 MHz
+	#define RCC_MSIRANGE_7                 MSI = 8 MHz
+	#define RCC_MSIRANGE_8                 MSI = 16 MHz
+	#define RCC_MSIRANGE_9                 MSI = 24 MHz
+	#define RCC_MSIRANGE_10                MSI = 32 MHz
+	#define RCC_MSIRANGE_11                MSI = 48 MHz   dont use this one*/
+  RCC_OscInitStruct.MSIState = RCC_MSI_ON;  //datasheet says NOT to turn on the MSI then change the frequency.
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
